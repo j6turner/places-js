e3a9ff255a6e7293abd857cdaa2f80fa6ae97bdc
Arsi <argoncode18@@gmail.com>
2015-02-16 16:49:49 -0800

    Add jQuery functionality to add and display Travelled Places

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
-  $("form#").submit(function(event) {
+  $("form#new-place").submit(function(event) {
     event.preventDefault();
 
+    var placeLocale = $("input#place-locale").val();
+    var placeDateStart = $("input#visit-start").val();
+    var placeDateEnd = $("input#visit-end").val();
+    var placeDescription = $("input#place-description").val();
+    var placeHighlights = $("input#place-highlights").val();
+    var newPlace = { placeLocale: placeLocale,
+                       travelTime: [placeDateStart, placeDateEnd],
+                         placeDescription: placeDescription,
+                           placeHighlights: placeHighlights };
 
-
-
-
+    $("ul#places-travelled").append("<li><span class=''>"
+                      + newPlace.placeLocale
+                        + "</span></li>");
   });
 });
